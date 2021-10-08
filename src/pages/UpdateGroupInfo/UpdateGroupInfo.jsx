2b11bd8c96912ef7de1fe499bdbb487bf087fa2c
Henry Gillard <henry@MacBook-Pro.local>
2021-10-08 11:32:56 -0700

    Year Select

--- src/pages/UpdateGroupInfo/UpdateGroupInfo.jsx
+++ src/pages/UpdateGroupInfo/UpdateGroupInfo.jsx
@@ -12,13 +12,26 @@ export default function UpdateGroupInfo({group, setGroup}) {
        {initData}
     );
 
+    const years = ["1972", '1973', '1974', '1975', '1976'
+    , '1977', '1978', '1979', '1980', '1981', '1982'
+    , '1983', '1984', '1985', '1986', '1987', '1988'
+    , '1989', '1990', '1991', '1992', '1993', '1994'
+    , '1995', '1996', '1997', '1998', '1999', '2000'
+    , '2001', '2002', '2003', '2004', '2005', '2006'
+    , '2007', '2008', '2009', '2010', '2011', '2012'
+    , '2013', '2014', '2015', '2016', '2017', '2018'
+    , '2019', '2020', '2021', '2022']
+    const sortYears = years.reverse()
+    const options = sortYears.map(y => <option value={y}>{y}</option>)
+
+
     const [error, setError] = useState('');
     const [selected, setSelected] = useState(false);
     
     function handleChange(evt) {
         const newGroupData= { ...groupData, [evt.target.name]: evt.target.value };
         setGroupData(newGroupData);
-        setError("No errors")
+        setError("")
       }
 
     async function handleSubmit(evt) {
@@ -30,11 +43,7 @@ export default function UpdateGroupInfo({group, setGroup}) {
         } catch {
             setError("This Video has already been submitted!");
         }
-        
-
     }
-
-    
 
     return(
         <>
@@ -42,6 +51,18 @@ export default function UpdateGroupInfo({group, setGroup}) {
         {selected ? 
         <form onSubmit={handleSubmit}>
             <div>
+                <label>Group: {group.name}</label>
+            </div>
+            <div>
+                <label>Year: </label>
+                <select 
+                value={groupData.years}
+                onChange={handleChange}
+                name="year">
+                    {options}
+                </select>
+            </div>
+            {/* <div>
                 Year: 
                 <input
                 name="year"
@@ -49,11 +70,9 @@ export default function UpdateGroupInfo({group, setGroup}) {
                 required
                 onChange={handleChange}
                 />
-                
-                
-            </div>
+            </div> */}
             <div>
-                Video URL: 
+                <label>Video URL: </label> 
                 <input
                 name="videoUrl"
                 value={groupData.videoUrl}
