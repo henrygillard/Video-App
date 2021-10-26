c3c55a52963e4de1457f41519846eb74f351d3f9
Henry Gillard <henry@MacBook-Pro.local>
2021-10-26 16:01:39 -0700

    some style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,8 @@ export default function NavBar() {
     return(
         <header>
             <h1>Marching Arts Database</h1>
-            <Link to="/"><h3>Home</h3></Link>
+            <img src="https://i.imgur.com/lm5NWj5.png" />
+            <Link className="link" to="/"><h3>Home</h3></Link>
         </header>
     )
 }
